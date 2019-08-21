5b65be3cda82c2a4c2566a46c78d77953d1e83f4
David Boike <david.boike@gmail.com>
2019-08-21 15:00:12 -0500

    Add approval tests

---
+++ src/AzureServiceBus.Tests/APIApprovals.cs
@@ -0,0 +1,15 @@
+﻿using NServiceBus.AzureFunctions.AzureServiceBus;
+using NUnit.Framework;
+using Particular.Approvals;
+using PublicApiGenerator;
+
+[TestFixture]
+public class APIApprovals
+{
+	[Test]
+	public void Approve()
+	{
+		var publicApi = ApiGenerator.GeneratePublicApi(typeof(AzureServiceBusTriggerEndpoint).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" });
+		Approver.Verify(publicApi);
+	}
+}
---
+++ src/AzureStorageQueues.Tests/APIApprovals.cs
@@ -0,0 +1,15 @@
+﻿using NServiceBus.AzureFunctions.AzureStorageQueues;
+using NUnit.Framework;
+using Particular.Approvals;
+using PublicApiGenerator;
+
+[TestFixture]
+public class APIApprovals
+{
+	[Test]
+	public void Approve()
+	{
+		var publicApi = ApiGenerator.GeneratePublicApi(typeof(AzureStorageQueueTriggerEndpoint).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" });
+		Approver.Verify(publicApi);
+	}
+}
