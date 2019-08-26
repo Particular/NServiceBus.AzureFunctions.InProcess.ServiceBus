62559c98489112b2229f6cd218857830cbb2b7b0
Sean Feldman <feldman.sean@gmail.com>
2019-08-26 12:31:41 -0600

    Update AzureFunctions to use the latest (0060) Serverless package

--- src/NServiceBus.AzureFunctions.ServiceBus/FunctionEndpoint.cs
+++ src/NServiceBus.AzureFunctions.ServiceBus/FunctionEndpoint.cs
@@ -14,12 +14,12 @@
     /// An NServiceBus endpoint hosted in Azure Function which does not receive messages automatically but only handles
     /// messages explicitly passed to it by the caller.
     /// </summary>
-    public class FunctionEndpoint : ServerlessEndpoint<ExecutionContext>
+    public class FunctionEndpoint : ServerlessEndpoint<ExecutionContext, ServiceBusTriggeredEndpointConfiguration>
     {
         /// <summary>
         /// Create a new endpoint hosting in Azure Function.
         /// </summary>
-        public FunctionEndpoint(Func<ExecutionContext, ServerlessEndpointConfiguration> configurationFactory) : base(configurationFactory)
+        public FunctionEndpoint(Func<ExecutionContext, ServiceBusTriggeredEndpointConfiguration> configurationFactory) : base(configurationFactory)
         {
         }
 
