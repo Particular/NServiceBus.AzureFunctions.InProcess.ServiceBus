99e074a1741b687086eb293d8818bc86e42af528
Tim Bussmann <timbussmann@users.noreply.github.com>
2019-08-26 17:35:19 +0200

    remove extension methods (#7)

--- src/NServiceBus.AzureFunctions.AzureStorageQueues/FunctionEndpoint.cs
+++ src/NServiceBus.AzureFunctions.AzureStorageQueues/FunctionEndpoint.cs
@@ -1,8 +1,16 @@
 ﻿namespace NServiceBus.AzureFunctions.AzureStorageQueues
 {
-    using Microsoft.Azure.WebJobs;
     using Serverless;
     using System;
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Azure.Transports.WindowsAzureStorageQueues;
+    using Extensibility;
+    using Microsoft.WindowsAzure.Storage.Queue;
+    using Newtonsoft.Json;
+    using Transport;
+    using ExecutionContext = Microsoft.Azure.WebJobs.ExecutionContext;
 
     /// <summary>
     /// An NServiceBus endpoint hosted in Azure Function which does not receive messages automatically but only handles
@@ -15,5 +23,26 @@
         /// </summary>
         public FunctionEndpoint(Func<ExecutionContext, ServerlessEndpointConfiguration> configurationFactory) : base(configurationFactory)
         {
-        }}
+        }
+
+        /// <summary>
+        /// Processes a message received from an AzureStorageQueue trigger using the NServiceBus message pipeline.
+        /// </summary>
+        public Task Process(CloudQueueMessage message, ExecutionContext executionContext)
+        {
+            var serializer = new JsonSerializer();
+            var msg = serializer.Deserialize<MessageWrapper>(
+                new JsonTextReader(new StreamReader(new MemoryStream(message.AsBytes))));
+
+            var messageContext = new MessageContext(
+                Guid.NewGuid().ToString("N"),
+                msg.Headers,
+                msg.Body,
+                new TransportTransaction(),
+                new CancellationTokenSource(),
+                new ContextBag());
+
+            return Process(messageContext, executionContext);
+        }
+    }
 }
