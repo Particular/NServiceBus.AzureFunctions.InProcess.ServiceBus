d55ca028c04c2bf9e2acd309da28cb225f4d20e0
David Boike <david.boike@gmail.com>
2019-08-27 08:30:01 -0500

    Remove using

--- src/NServiceBus.AzureFunctions.StorageQueues/FunctionEndpoint.cs
+++ src/NServiceBus.AzureFunctions.StorageQueues/FunctionEndpoint.cs
@@ -1,7 +1,6 @@
 ﻿namespace NServiceBus.AzureFunctions.StorageQueues
 {
     using System;
-    using System.Collections.Generic;
     using System.IO;
     using System.Threading;
     using System.Threading.Tasks;
