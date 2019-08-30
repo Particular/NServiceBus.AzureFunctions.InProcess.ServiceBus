5be64d46848fee9492f6161d79a990444d96b431
Tim Bussmann <timbussmann@users.noreply.github.com>
2019-08-30 11:10:27 +0200

    derive configuration from trigger attributes

--- src/NServiceBus.AzureFunctions.ServiceBus/ServiceBusTriggeredEndpointConfiguration.cs
+++ src/NServiceBus.AzureFunctions.ServiceBus/ServiceBusTriggeredEndpointConfiguration.cs
@@ -1,7 +1,9 @@
 ﻿namespace NServiceBus.AzureFunctions.ServiceBus
 {
-    using Microsoft.Extensions.Logging;
-    using Logging;
+    using System;
+    using System.Diagnostics;
+    using System.Reflection;
+    using Microsoft.Azure.WebJobs;
     using Serverless;
 
     /// <summary>
@@ -9,6 +11,8 @@
     /// </summary>
     public class ServiceBusTriggeredEndpointConfiguration : ServerlessEndpointConfiguration
     {
+        const string DefaultServiceBusConnectionName = "AzureWebJobsServiceBus";
+
         /// <summary>
         /// Azure Service Bus transport
         /// </summary>
@@ -23,7 +27,7 @@
         {
             Transport = UseTransport<AzureServiceBusTransport>();
 
-            var connectionString = System.Environment.GetEnvironmentVariable(connectionStringName);
+            var connectionString = Environment.GetEnvironmentVariable(connectionStringName ?? DefaultServiceBusConnectionName);
             Transport.ConnectionString(connectionString);
 
             var recoverability = AdvancedConfiguration.Recoverability();
@@ -32,6 +36,45 @@
 
             FunctionsLoggerFactory = new FunctionsLoggerFactory(logger);
             LogManager.UseFactory(FunctionsLoggerFactory);
+        }
+
+        /// <summary>
+        /// Attempts to derive the required configuration parameters automatically from the Azure functions related attributes via reflection.
+        /// </summary>
+        public static ServiceBusTriggeredEndpointConfiguration AutoConfigure()
+        {
+            var configuration = TryGetTriggerConfiguration();
+            if (configuration != null)
+            {
+                return new ServiceBusTriggeredEndpointConfiguration(configuration.QueueName, configuration.Connection);
+            }
+
+            throw new Exception($"Unable to automatically derive the endpoint name from the ServiceBusTrigger attribute. Make sure the attribute exists or create the {nameof(ServiceBusTriggeredEndpointConfiguration)} with the required parameter manually.");
+
+            ServiceBusTriggerAttribute TryGetTriggerConfiguration()
+            {
+                var frames = new StackTrace().GetFrames();
+                foreach (var stackFrame in frames)
+                {
+                    var method = stackFrame.GetMethod();
+                    var functionAttribute = method.GetCustomAttribute<FunctionNameAttribute>(false);
+                    if (functionAttribute != null)
+                    {
+                        foreach (var parameter in method.GetParameters())
+                        {
+                            var triggerConfiguration = parameter.GetCustomAttribute<ServiceBusTriggerAttribute>(false);
+                            if (triggerConfiguration != null)
+                            {
+                                return triggerConfiguration;
+                            }
+                        }
+                    }
+
+                    return null;
+                }
+
+                return null;
+            }
         }
     }
 }
--- src/NServiceBus.AzureFunctions.StorageQueues/StorageQueueTriggeredEndpointConfiguration.cs
+++ src/NServiceBus.AzureFunctions.StorageQueues/StorageQueueTriggeredEndpointConfiguration.cs
@@ -2,6 +2,10 @@
 {
     using Logging;
     using Microsoft.Extensions.Logging;
+	using System;
+    using System.Diagnostics;
+    using System.Reflection;
+    using Microsoft.Azure.WebJobs;
     using Serverless;
 
     /// <summary>
@@ -9,6 +13,8 @@
     /// </summary>
     public class StorageQueueTriggeredEndpointConfiguration : ServerlessEndpointConfiguration
     {
+        const string DefaultStorageConnectionString = "AzureWebJobsStorage";
+
         /// <summary>
         /// Azure Storage Queues transport
         /// </summary>
@@ -19,11 +25,11 @@
         /// <summary>
         /// Creates a serverless NServiceBus endpoint running within an AzureStorageQueue trigger.
         /// </summary>
-        public StorageQueueTriggeredEndpointConfiguration(string endpointName, ILogger logger, string connectionStringName = "AzureWebJobsStorage") : base(endpointName)
+        public StorageQueueTriggeredEndpointConfiguration(string endpointName, string connectionStringName = "AzureWebJobsStorage") : base(endpointName)
         {
             Transport = UseTransport<AzureStorageQueueTransport>();
 
-            var connectionString = System.Environment.GetEnvironmentVariable(connectionStringName);
+            var connectionString = Environment.GetEnvironmentVariable(connectionStringName ?? DefaultStorageConnectionString);
             Transport.ConnectionString(connectionString);
 
             var recoverability = AdvancedConfiguration.Recoverability();
@@ -32,6 +38,45 @@
 
             FunctionsLoggerFactory = new FunctionsLoggerFactory(logger);
             LogManager.UseFactory(FunctionsLoggerFactory);
+        }
+
+        /// <summary>
+        /// Attempts to derive the required configuration parameters automatically from the Azure functions related attributes via reflection.
+        /// </summary>
+        public static StorageQueueTriggeredEndpointConfiguration AutoConfigure()
+        {
+            var configuration = TryGetTriggerConfiguration();
+            if (configuration != null)
+            {
+                return new StorageQueueTriggeredEndpointConfiguration(configuration.QueueName, configuration.Connection);
+            }
+
+            throw new Exception($"Unable to automatically derive the endpoint name from the QueueTrigger attribute. Make sure the attribute exists or create the {nameof(StorageQueueTriggeredEndpointConfiguration)} with the required parameter manually.");
+
+            QueueTriggerAttribute TryGetTriggerConfiguration()
+            {
+                var frames = new StackTrace().GetFrames();
+                foreach (var stackFrame in frames)
+                {
+                    var method = stackFrame.GetMethod();
+                    var functionAttribute = method.GetCustomAttribute<FunctionNameAttribute>(false);
+                    if (functionAttribute != null)
+                    {
+                        foreach (var parameter in method.GetParameters())
+                        {
+                            var triggerConfiguration = parameter.GetCustomAttribute<QueueTriggerAttribute>(false);
+                            if (triggerConfiguration != null)
+                            {
+                                return triggerConfiguration;
+                            }
+                        }
+                    }
+
+                    return null;
+                }
+
+                return null;
+            }
         }
     }
 }
