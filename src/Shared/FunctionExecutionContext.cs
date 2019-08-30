352e27005c19289eaf36da89876fcdba1ef7d932
Sean Feldman <feldman.sean@gmail.com>
2019-08-30 11:09:50 -0600

    XML comments on public type

--- src/Shared/FunctionExecutionContext.cs
+++ src/Shared/FunctionExecutionContext.cs
@@ -4,15 +4,27 @@ namespace NServiceBus.AzureFunctions
     using Microsoft.Extensions.Logging;
     using Microsoft.Extensions.Logging.Abstractions;
 
+    /// <summary>
+    /// Function execution context
+    /// </summary>
     public class FunctionExecutionContext
     {
+        /// <summary>
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="logger"></param>
         public FunctionExecutionContext(ExecutionContext context, ILogger logger = null)
         {
             Logger = logger ?? NullLogger.Instance;
             Context = context;
         }
 
+        /// <summary>
+        /// </summary>
         public ExecutionContext Context { get; }
+        
+        /// <summary>
+        /// </summary>
         public ILogger Logger { get; }
     }
 }
