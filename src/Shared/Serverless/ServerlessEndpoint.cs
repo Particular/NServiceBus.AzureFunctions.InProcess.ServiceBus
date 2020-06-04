6e9f189c5f02bb3f21326e82f2ecb6fc35c28a78
Tim Bussmann <timbussmann@users.noreply.github.com>
2020-06-04 14:55:48 +0200

    add basic assembly scanning logic

--- src/Shared/Serverless/ServerlessEndpoint.cs
+++ src/Shared/Serverless/ServerlessEndpoint.cs
@@ -1,8 +1,12 @@
 ﻿namespace NServiceBus.AzureFunctions
 {
     using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Runtime.Loader;
     using System.Threading;
     using System.Threading.Tasks;
+    using Microsoft.Extensions.Logging;
     using Transport;
 
     /// <summary>
@@ -12,6 +16,7 @@
     /// </summary>
     public abstract class ServerlessEndpoint<TExecutionContext, TConfiguration>
         where TConfiguration : ServerlessEndpointConfiguration
+        where TExecutionContext : FunctionExecutionContext
     {
         /// <summary>
         /// Create a new session based on the configuration factory provided.
@@ -45,9 +50,37 @@
         /// Thread-safe initialization method that allows to get access to the strongly typed configuration.
         /// </summary>
         /// <remarks>Will only be called once either when <see cref="Process"/>, <see cref="ProcessFailedMessage"/> or <see cref="InitializeEndpointIfNecessary"/> is called.</remarks>
+        /// <param name="executionContext">The exection context.</param>
         /// <param name="configuration">The fully initialized configuration.</param>
-        protected virtual Task Initialize(TConfiguration configuration)
+        protected virtual Task Initialize(TExecutionContext executionContext, TConfiguration configuration)
         {
+            //TODO: also support exe files like core?
+            var binFiles = Directory.EnumerateFiles(
+                Path.Combine(executionContext.ExecutionContext.FunctionAppDirectory, "bin"), 
+                "*.dll", 
+                SearchOption.TopDirectoryOnly);
+
+            var assemblyLoadContext = AssemblyLoadContext.GetLoadContext(Assembly.GetExecutingAssembly());
+            foreach (var binFile in binFiles)
+            {
+                try
+                {
+                    var assemblyName = AssemblyName.GetAssemblyName(binFile);
+                    if (IsRuntimeAssembly(assemblyName.GetPublicKeyToken()))
+                    {
+                        continue;
+                    }
+
+                    //TODO: skip Particular signed assemblies too?
+
+                    assemblyLoadContext.LoadFromAssemblyName(assemblyName);
+                }
+                catch (Exception e)
+                {
+                    executionContext.Logger.LogDebug(e, "Failed to load assembly {0}. This error can be ignored if the assembly isn't required to execute the function.", binFile);
+                }
+            }
+
             return Task.CompletedTask;
         }
 
@@ -67,7 +100,7 @@
                     if (pipeline == null)
                     {
                         var configuration = configurationFactory(executionContext);
-                        await Initialize(configuration).ConfigureAwait(false);
+                        await Initialize(executionContext, configuration).ConfigureAwait(false);
                         await Endpoint.Start(configuration.EndpointConfiguration).ConfigureAwait(false);
 
                         pipeline = configuration.PipelineInvoker;
@@ -78,6 +111,45 @@
                     semaphoreLock.Release();
                 }
             }
+        }
+
+        static bool IsRuntimeAssembly(byte[] publicKeyToken)
+        {
+            var tokenString = BitConverter.ToString(publicKeyToken).Replace("-", string.Empty).ToLowerInvariant();
+
+            //Compare token to known Microsoft tokens
+
+            if (tokenString == "b77a5c561934e089")
+            {
+                return true;
+            }
+
+            if (tokenString == "7cec85d7bea7798e")
+            {
+                return true;
+            }
+
+            if (tokenString == "b03f5f7f11d50a3a")
+            {
+                return true;
+            }
+
+            if (tokenString == "31bf3856ad364e35")
+            {
+                return true;
+            }
+
+            if (tokenString == "cc7b13ffcd2ddd51")
+            {
+                return true;
+            }
+
+            if (tokenString == "adb9793829ddae60")
+            {
+                return true;
+            }
+
+            return false;
         }
 
         readonly Func<TExecutionContext, TConfiguration> configurationFactory;
