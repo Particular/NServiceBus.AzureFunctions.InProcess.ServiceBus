c8601dee92384baca8b30436f1ea9efd9dccbb25
Tim Bussmann <timbussmann@users.noreply.github.com>
2020-06-05 14:41:58 +0200

    add some notes

--- src/Testing.Handlers/DummyMessageHandler.cs
+++ src/Testing.Handlers/DummyMessageHandler.cs
@@ -3,6 +3,13 @@
     using System.Threading.Tasks;
     using NServiceBus;
 
+    /**
+     * This types are used to test the assembly scanning logic which is required to load additional handler assemblies
+     * which are optimized away when they calling code doesn't explicitly reference a type from the assembly.
+     * To update the assembly used by the tests, rebuild this project and replace the dll which has been copied to the
+     * test projects manually.
+     * Referencing this project from the test project doesn't work as Core's assembly scanning mechanism will find and load it.
+     */
     public class DummyMessageHandler : IHandleMessages<DummyMessage>
     {
         public Task Handle(DummyMessage message, IMessageHandlerContext context)
