c025747c771c81b02d72f43b8b90b3c7d7af3156
Sean Feldman <feldman.sean@gmail.com>
2020-06-10 16:56:58 -0600

    C# 7.3 rule

--- src/Shared/Serverless/ServerlessEndpointConfiguration.cs
+++ src/Shared/Serverless/ServerlessEndpointConfiguration.cs
@@ -77,11 +77,13 @@
         public static Guid Create(string data)
         {
             // use MD5 hash to get a 16-byte hash of the string
-            using var provider = new MD5CryptoServiceProvider();
-            var inputBytes = Encoding.Default.GetBytes(data);
-            var hashBytes = provider.ComputeHash(inputBytes);
-            // generate a guid from the hash:
-            return new Guid(hashBytes);
+            using (var provider = new MD5CryptoServiceProvider())
+            {
+                var inputBytes = Encoding.Default.GetBytes(data);
+                var hashBytes = provider.ComputeHash(inputBytes);
+                // generate a guid from the hash:
+                return new Guid(hashBytes);
+            }
         }
     }
 }
