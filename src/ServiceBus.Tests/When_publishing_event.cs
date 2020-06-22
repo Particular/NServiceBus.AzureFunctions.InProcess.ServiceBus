aa2032079a17f20d289bf794fc98934d3aa298a7
Tim Bussmann <timbussmann@users.noreply.github.com>
2020-06-22 14:14:47 +0200

    add publishing acceptance test

---
+++ src/ServiceBus.Tests/When_publishing_event.cs
@@ -0,0 +1,75 @@
+﻿namespace ServiceBus.Tests
+{
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.AcceptanceTesting;
+    using NUnit.Framework;
+
+    public class When_publishing_event
+    {
+        [Test]
+        public async Task Should_publish_to_subscribers()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<SubscriberEndpoint>()
+                .WithComponent(new PublishingFunction())
+                .Done(c => c.EventReceived)
+                .Run();
+
+            Assert.IsTrue(context.EventReceived);
+        }
+
+        class Context : ScenarioContext
+        {
+            public bool EventReceived { get; set; }
+        }
+
+        class SubscriberEndpoint : EndpointConfigurationBuilder
+        {
+            public SubscriberEndpoint()
+            {
+                EndpointSetup<DefaultEndpoint>();
+            }
+
+            public class EventHandler : IHandleMessages<TestEvent>
+            {
+                Context testContext;
+
+                public EventHandler(Context testContext)
+                {
+                    this.testContext = testContext;
+                }
+
+                public Task Handle(TestEvent message, IMessageHandlerContext context)
+                {
+                    testContext.EventReceived = true;
+                    return Task.CompletedTask;
+                }
+            }
+        }
+
+        class PublishingFunction : FunctionEndpointComponent
+        {
+            public PublishingFunction()
+            {
+                Messages.Add(new TriggerMessage());
+            }
+
+            public class PublishingHandler : IHandleMessages<TriggerMessage>
+            {
+                public Task Handle(TriggerMessage message, IMessageHandlerContext context)
+                {
+                    return context.Publish(new TestEvent());
+                }
+            }
+        }
+
+        class TriggerMessage : IMessage
+        {
+        }
+
+        class TestEvent : IEvent
+        {
+        }
+    }
+}
