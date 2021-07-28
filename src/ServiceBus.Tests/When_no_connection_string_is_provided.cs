73a279db02a24f172d37b2d3856170aa4638cdf3
Mike Minutillo <mike.minutillo@particular.net>
2021-07-28 11:48:54 +0800

    Detect common problems

---
+++ src/ServiceBus.Tests/When_no_connection_string_is_provided.cs
@@ -0,0 +1,26 @@
+﻿namespace ServiceBus.Tests
+{
+    using System;
+    using NServiceBus;
+    using NServiceBus.AzureFunctions.InProcess.ServiceBus;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class When_no_connection_string_is_provided
+    {
+        [Test]
+        public void Should_guide_user_towards_success()
+        {
+            var endpointConfiguration = new EndpointConfiguration("SampleEndpoint");
+            endpointConfiguration.UseTransport<ServerlessTransport<AzureServiceBusTransport>>();
+
+            var exception = Assert.ThrowsAsync<Exception>(
+                () => Endpoint.Create(endpointConfiguration),
+                "Exception should be thrown at endpoint creation so that the error will be found during functions startup"
+            );
+
+            StringAssert.Contains(".Transport.ConnectionString(", exception?.Message, "Should mention the transport extension approach");
+            StringAssert.Contains("environment variable", exception?.Message, "Should mention the environment variable approach");
+        }
+    }
+}
---
+++ src/ServiceBus.Tests/When_providing_missing_connection_string_name.cs
@@ -0,0 +1,22 @@
+﻿namespace ServiceBus.Tests
+{
+    using System;
+    using NServiceBus;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class When_providing_missing_connection_string_name
+    {
+        [Test]
+        public void Should_guide_user_towards_success()
+        {
+            var exception = Assert.Throws<Exception>(() =>
+                    new ServiceBusTriggeredEndpointConfiguration("SampleEndpoint", "DOES_NOT_EXIST"),
+                "Exception should be thrown in constructor so that the error will be found during functions startup"
+            );
+
+            StringAssert.Contains("environment variable", exception?.Message, "Should mention that there's a missing environment variable");
+            StringAssert.Contains("DOES_NOT_EXIST", exception?.Message, "Should mention the specific environment variable");
+        }
+    }
+}
