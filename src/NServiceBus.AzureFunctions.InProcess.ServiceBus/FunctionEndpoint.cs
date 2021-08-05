644a3c8c272bf801a4ee1bc63d74b9c1e0121756
Mike Minutillo <mike.minutillo@particular.net>
2021-08-05 15:47:42 +0800

    Switch to functions logger before flushing

--- src/NServiceBus.AzureFunctions.InProcess.ServiceBus/FunctionEndpoint.cs
+++ src/NServiceBus.AzureFunctions.InProcess.ServiceBus/FunctionEndpoint.cs
@@ -34,6 +34,7 @@
         public async Task Process(Message message, ExecutionContext executionContext, ILogger functionsLogger = null)
         {
             FunctionsLoggerFactory.Instance.SetCurrentLogger(functionsLogger);
+            LogManager.UseFactory(FunctionsLoggerFactory.Instance);
             DeferredLoggerFactory.Instance.FlushAll(FunctionsLoggerFactory.Instance);
 
             var messageContext = CreateMessageContext(message);
