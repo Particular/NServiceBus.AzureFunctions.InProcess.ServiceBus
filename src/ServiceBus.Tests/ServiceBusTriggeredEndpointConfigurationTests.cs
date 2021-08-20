cb098179ce0ebaec30ad7f5029eead53d2486a8b
Mike Minutillo <mike.minutillo@particular.net>
2021-08-20 17:31:48 +0800

    Test callback order

---
+++ src/ServiceBus.Tests/ServiceBusTriggeredEndpointConfigurationTests.cs
@@ -0,0 +1,29 @@
+﻿namespace ServiceBus.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.Extensions.Configuration;
+    using NServiceBus;
+    using NUnit.Framework;
+    using Particular.Approvals;
+
+    [TestFixture]
+    class ServiceBusTriggeredEndpointConfigurationTests
+    {
+        [Test]
+        public void ConfigurationCallbackMethodOrder()
+        {
+            var config = new ServiceBusTriggeredEndpointConfiguration("MyEndpoint", default(IConfiguration));
+            var callOrder = new List<string>();
+
+            config.Advanced(cfg => callOrder.Add(nameof(ServiceBusTriggeredEndpointConfiguration.Advanced)));
+            config.Routing(routing => callOrder.Add(nameof(ServiceBusTriggeredEndpointConfiguration.Routing)));
+            config.ConfigureTransport(transport => callOrder.Add(nameof(ServiceBusTriggeredEndpointConfiguration.ConfigureTransport)));
+            config.UseSerialization<NewtonsoftSerializer>(serialization => callOrder.Add(nameof(ServiceBusTriggeredEndpointConfiguration.UseSerialization)));
+
+            config.CreateEndpointConfiguration();
+
+            Approver.Verify(string.Join(Environment.NewLine, callOrder));
+        }
+    }
+}
