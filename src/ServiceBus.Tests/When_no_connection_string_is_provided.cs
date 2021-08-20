26278a72cf7f70fa6ce679bad2fdb4ecc87dd523
Mike Minutillo <mike.minutillo@particular.net>
2021-08-20 13:51:48 +0800

    Properly detect when no connection string is created
    And guide user towards ways to fix it

---
+++ src/ServiceBus.Tests/When_no_connection_string_is_provided.cs
@@ -0,0 +1,38 @@
+﻿namespace ServiceBus.Tests
+{
+    using System;
+    using Microsoft.Extensions.Configuration;
+    using NServiceBus;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class When_no_connection_string_is_provided
+    {
+        [Test]
+        public void Should_guide_user_towards_success()
+        {
+            var defaultConnectionStringKey = ServiceBusTriggeredEndpointConfiguration.DefaultServiceBusConnectionName;
+            var connectionString = Environment.GetEnvironmentVariable(defaultConnectionStringKey);
+
+            try
+            {
+                Environment.SetEnvironmentVariable(defaultConnectionStringKey, null, EnvironmentVariableTarget.Process);
+                var serviceBusTriggeredEndpointConfiguration =
+                    new ServiceBusTriggeredEndpointConfiguration("SampleEndpoint", default(IConfiguration));
+
+                var exception = Assert.Throws<Exception>(
+                    () => serviceBusTriggeredEndpointConfiguration.CreateEndpointConfiguration(),
+                    "Exception should be thrown at endpoint creation so that the error will be found during functions startup"
+                );
+
+                StringAssert.Contains(".ServiceBusConnectionString(", exception?.Message, "Should mention the code-first approach");
+                StringAssert.Contains("environment variable", exception?.Message, "Should mention the environment variable approach");
+            }
+            finally
+            {
+                Environment.SetEnvironmentVariable(defaultConnectionStringKey, connectionString);
+            }
+
+        }
+    }
+}
