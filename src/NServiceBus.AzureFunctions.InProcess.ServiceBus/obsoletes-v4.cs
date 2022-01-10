ec25d3a0b925803d27d7cd681642dcb40966211e
Andreas Öhlund <andreas.ohlund@particular.net>
2022-01-10 14:08:09 +0100

    Bump obsoletes to v4 (#400)

---
+++ src/NServiceBus.AzureFunctions.InProcess.ServiceBus/obsoletes-v4.cs
@@ -0,0 +1,12 @@
+﻿#pragma warning disable 1591
+
+namespace NServiceBus
+{
+    public partial class ServiceBusTriggeredEndpointConfiguration
+    {
+        [ObsoleteEx(ReplacementTypeOrMember = "UseNServiceBus(ENDPOINTNAME, CONNECTIONSTRING)",
+                    TreatAsErrorFromVersion = "4",
+                    RemoveInVersion = "5")]
+        public string ServiceBusConnectionString { get; set; }
+    }
+}
