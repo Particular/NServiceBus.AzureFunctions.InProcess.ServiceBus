595561c1e8243c42f91a6ad8a121a494afcf69a2
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-03-15 15:38:10 +0100

    typo

--- src/NServiceBus.AzureFunctions.InProcess.ServiceBus/Serverless/TransportWrapper/PipelineInvoker.cs
+++ src/NServiceBus.AzureFunctions.InProcess.ServiceBus/Serverless/TransportWrapper/PipelineInvoker.cs
@@ -28,7 +28,7 @@
 
         public Task StartReceive(CancellationToken cancellationToken) => Task.CompletedTask;
 
-        // Noop because the rate at which Azure Functions pushes messages to the pipeline can't be controlled.
+        // No-op because the rate at which Azure Functions pushes messages to the pipeline can't be controlled.
         public Task ChangeConcurrency(PushRuntimeSettings limitations, CancellationToken cancellationToken = new CancellationToken()) => Task.CompletedTask;
 
         public Task StopReceive(CancellationToken cancellationToken) => Task.CompletedTask;
