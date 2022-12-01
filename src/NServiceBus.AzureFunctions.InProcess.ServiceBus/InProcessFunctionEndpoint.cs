8bec0f2253b05b0b09e0818d043816e1761e4510
Andreas Öhlund <andreas.ohlund@particular.net>
2022-12-01 14:10:23 +0100

    Exclude unscanable Microsoft.Identity.Client* dlls

--- src/NServiceBus.AzureFunctions.InProcess.ServiceBus/InProcessFunctionEndpoint.cs
+++ src/NServiceBus.AzureFunctions.InProcess.ServiceBus/InProcessFunctionEndpoint.cs
@@ -239,7 +239,9 @@
             "Grpc.Net.ClientFactory.dll",
             "Azure.Identity.dll",
             "Microsoft.Extensions.Azure.dll",
-            "NServiceBus.Extensions.DependencyInjection.dll"
+            "NServiceBus.Extensions.DependencyInjection.dll",
+            "Microsoft.Identity.Client.dll",
+            "Microsoft.Identity.Client.Extensions.Msal.dll"
         };
 
         internal async Task InitializeEndpointIfNecessary(ExecutionContext executionContext, ILogger logger, CancellationToken cancellationToken)
