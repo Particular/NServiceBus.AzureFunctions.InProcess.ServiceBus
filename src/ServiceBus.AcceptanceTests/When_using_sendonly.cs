852de6e8433e78f2f40c09984b20725d2d138c8e
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-02-13 18:44:17 +0100

    acceptance test

---
+++ src/ServiceBus.AcceptanceTests/When_using_sendonly.cs
@@ -0,0 +1,67 @@
+﻿namespace ServiceBus.Tests
+{
+    using System.Threading.Tasks;
+    using Microsoft.Azure.WebJobs;
+    using NServiceBus;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.AcceptanceTesting.Customization;
+    using NUnit.Framework;
+
+    public class When_using_sendonly
+    {
+        [Test]
+        public async Task Should_send_messages()
+        {
+            await Scenario.Define<Context>()
+                .WithEndpoint<ReceivingEndpoint>()
+                .WithComponent(new SendOnlyFunction())
+                .Done(c => c.HandlerReceivedMessage)
+                .Run();
+        }
+
+        class Context : ScenarioContext
+        {
+            public bool HandlerReceivedMessage { get; set; }
+        }
+
+        class ReceivingEndpoint : EndpointConfigurationBuilder
+        {
+            public ReceivingEndpoint() => EndpointSetup<DefaultEndpoint>();
+
+            public class TestMessageHandler : IHandleMessages<TestMessage>
+            {
+                readonly Context testContext;
+
+                public TestMessageHandler(Context testContext) => this.testContext = testContext;
+
+                public Task Handle(TestMessage message, IMessageHandlerContext context)
+                {
+                    testContext.HandlerReceivedMessage = true;
+                    return Task.CompletedTask;
+                }
+            }
+        }
+
+        class SendOnlyFunction : SimpleTriggerFunctionComponent
+        {
+            public SendOnlyFunction()
+            {
+                CustomizeConfiguration = configuration =>
+                {
+                    configuration.AdvancedConfiguration.SendOnly();
+
+                    configuration.Routing.RouteToEndpoint(typeof(TestMessage), typeof(ReceivingEndpoint));
+                };
+            }
+
+            public override Task TriggerAction(IFunctionEndpoint endpoint, ExecutionContext executionContext)
+            {
+                return endpoint.Send(new TestMessage(), executionContext);
+            }
+        }
+
+        class TestMessage : IMessage
+        {
+        }
+    }
+}
