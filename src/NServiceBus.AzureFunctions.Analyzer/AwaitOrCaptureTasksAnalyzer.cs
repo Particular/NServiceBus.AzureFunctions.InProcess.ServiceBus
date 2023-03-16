8ad3312c5005fe58897c9d8b524d3af74b27389f
Tomek Masternak <tomasz.masternak@particular.net>
2023-03-16 18:36:33 +0100

    analyzer scafolding

---
+++ src/NServiceBus.AzureFunctions.Analyzer/AwaitOrCaptureTasksAnalyzer.cs
@@ -0,0 +1,104 @@
+namespace NServiceBus.Core.Analyzer
+{
+    using System;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AwaitOrCaptureTasksAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "NSB0001";
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(diagnostic);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.RegisterSyntaxNodeAction(Analyze, SyntaxKind.InvocationExpression);
+        }
+
+        static void Analyze(SyntaxNodeAnalysisContext context)
+        {
+            //if (!(context.Node is InvocationExpressionSyntax call))
+            //{
+            //    return;
+            //}
+
+            //if (!(call.Parent is ExpressionStatementSyntax))
+            //{
+            //    return;
+            //}
+
+            //foreach (var token in call.Expression?.DescendantTokens() ?? Enumerable.Empty<SyntaxToken>())
+            //{
+            //    if (context.CancellationToken.IsCancellationRequested)
+            //    {
+            //        return;
+            //    }
+
+            //    // check syntax tree (cheap) first for possible call requiring await and then check semantic model (expensive) to confirm
+            //    if (CouldBeMethodRequiringAwait(token) && IsMethodRequiringAwait(call, context))
+            //    {
+            //        context.ReportDiagnostic(Diagnostic.Create(diagnostic, call.GetLocation(), call.ToString()));
+            //        return;
+            //    }
+            //}
+        }
+
+        //static bool CouldBeMethodRequiringAwait(SyntaxToken syntaxToken) =>
+        //    syntaxToken.Kind() == SyntaxKind.IdentifierToken && methodNames.Contains(syntaxToken.Text);
+
+        //static bool IsMethodRequiringAwait(ExpressionSyntax call, SyntaxNodeAnalysisContext context) =>
+        //    context.SemanticModel.GetSymbolInfo(call, context.CancellationToken).Symbol is IMethodSymbol methodSymbol &&
+        //    methods.Contains(methodSymbol.GetFullName());
+
+        static readonly DiagnosticDescriptor diagnostic = new DiagnosticDescriptor(
+            DiagnosticId,
+            "Await Task",
+            "A Task returned by an async NServiceBus method is not awaited.",
+            "NServiceBus.Code",
+            DiagnosticSeverity.Error,
+            true,
+            "Tasks returned by NServiceBus methods must be awaited. Failure to await these Tasks may result in message loss. If the Task is assigned to a variable, this diagnostic is not shown, but the Task must still be awaited.");
+
+        // UniformSession is the only downstream package which can be supported by this analyzer.
+        // All other downstream packages must provide their own analyzer.
+        //static readonly ImmutableHashSet<string> methods = ImmutableHashSet.Create(
+        //    StringComparer.Ordinal,
+        //    "NServiceBus.IPipelineContext.Send",
+        //    "NServiceBus.IPipelineContext.Publish",
+        //    "NServiceBus.PipelineContextExtensions.Send",
+        //    "NServiceBus.PipelineContextExtensions.SendLocal",
+        //    "NServiceBus.PipelineContextExtensions.Publish",
+        //    "NServiceBus.IMessageSession.Send",
+        //    "NServiceBus.IMessageSession.Publish",
+        //    "NServiceBus.IMessageSession.Subscribe",
+        //    "NServiceBus.IMessageSession.Unsubscribe",
+        //    "NServiceBus.MessageSessionExtensions.Send",
+        //    "NServiceBus.MessageSessionExtensions.SendLocal",
+        //    "NServiceBus.MessageSessionExtensions.Publish",
+        //    "NServiceBus.MessageSessionExtensions.Subscribe",
+        //    "NServiceBus.MessageSessionExtensions.Unsubscribe",
+        //    "NServiceBus.IMessageProcessingContext.Reply",
+        //    "NServiceBus.IMessageProcessingContext.ForwardCurrentMessageTo",
+        //    "NServiceBus.MessageProcessingContextExtensions.Reply",
+        //    "NServiceBus.Saga.RequestTimeout",
+        //    "NServiceBus.Saga.ReplyToOriginator",
+        //    "NServiceBus.Endpoint.Create",
+        //    "NServiceBus.Endpoint.Start",
+        //    "NServiceBus.IStartableEndpoint.Start",
+        //    "NServiceBus.IEndpointInstance.Stop",
+        //    "NServiceBus.UniformSession.IUniformSession.Send",
+        //    "NServiceBus.UniformSession.IUniformSession.Publish",
+        //    "NServiceBus.UniformSession.UniformSessionExtensions.Send",
+        //    "NServiceBus.UniformSession.UniformSessionExtensions.SendLocal",
+        //    "NServiceBus.UniformSession.UniformSessionExtensions.Publish");
+
+        //static readonly ImmutableHashSet<string> methodNames = methods.Select(m => m.Split('.').Last()).ToImmutableHashSet();
+    }
+}
---
+++ src/NServiceBus.AzureFunctions.Analyzer/SymbolExtensions.cs
@@ -0,0 +1,138 @@
+﻿namespace NServiceBus.Core.Analyzer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.CodeAnalysis;
+
+    static class SymbolExtensions
+    {
+        public static string GetFullName(this IMethodSymbol method)
+        {
+            var tokens = new Stack<string>();
+            tokens.Push(method.Name);
+
+            var type = method.ContainingType;
+            while (type != null)
+            {
+                tokens.Push(type.Name);
+                type = type.ContainingType;
+            }
+
+            var @namespace = method.ContainingType.ContainingNamespace;
+            while (!string.IsNullOrEmpty(@namespace?.Name))
+            {
+                tokens.Push(@namespace.Name);
+                @namespace = @namespace.ContainingNamespace;
+            }
+
+            return string.Join(".", tokens);
+        }
+
+        public static bool Implements(this ITypeSymbol type, INamedTypeSymbol nonGenericInterface) =>
+            type != null &&
+            nonGenericInterface != null &&
+            (type.Equals(nonGenericInterface, SymbolEqualityComparer.IncludeNullability) || type.AllInterfaces.Any(candidate => candidate.Equals(nonGenericInterface, SymbolEqualityComparer.IncludeNullability)));
+
+        public static bool Extends(this IMethodSymbol method, INamedTypeSymbol nonGenericType)
+        {
+            if (method == null || nonGenericType == null)
+            {
+                return false;
+            }
+
+            if (!method.IsExtensionMethod)
+            {
+                return false;
+            }
+
+            if (!(method.ReducedFrom is IMethodSymbol extensionMethod))
+            {
+                return false;
+            }
+
+            if (!(extensionMethod.Parameters.FirstOrDefault() is IParameterSymbol thisParam))
+            {
+                return false;
+            }
+
+            return thisParam.Type.Equals(nonGenericType, SymbolEqualityComparer.IncludeNullability);
+        }
+
+        public static ITypeSymbol GetTypeSymbolOrDefault(this ISymbol symbol)
+        {
+            switch (symbol)
+            {
+                case IDiscardSymbol symbolWithType:
+                    return symbolWithType.Type;
+                case IEventSymbol symbolWithType:
+                    return symbolWithType.Type;
+                case IFieldSymbol symbolWithType:
+                    return symbolWithType.Type;
+                case ILocalSymbol symbolWithType:
+                    return symbolWithType.Type;
+                case IMethodSymbol symbolWithType:
+                    return symbolWithType.ReturnType;
+                case INamedTypeSymbol symbolWithType:
+                    return symbolWithType;
+                case IParameterSymbol symbolWithType:
+                    return symbolWithType.Type;
+                case IPointerTypeSymbol symbolWithType:
+                    return symbolWithType.PointedAtType;
+                case IPropertySymbol symbolWithType:
+                    return symbolWithType.Type;
+                case ITypeSymbol symbolWithType:
+                    return symbolWithType;
+                default:
+                    return null;
+            }
+        }
+
+        public static IEnumerable<ITypeSymbol> BaseTypesAndSelf(this ITypeSymbol type)
+        {
+            yield return type;
+
+            for (
+                var baseType = type.BaseType;
+                baseType != null;
+                baseType = baseType.BaseType)
+            {
+                yield return baseType;
+            }
+        }
+
+        public static bool IsSystemObjectType(this ITypeSymbol type) => type.SpecialType == SpecialType.System_Object;
+
+        // TODO: cater for generics, including in/out - test what CA2016 does in .NET 6
+        // after upgrading to Microsoft.CodeAnalysis.CSharp.Workspaces 3.x or later, we can convert this method to this single expression
+        // see https://github.com/dotnet/roslyn-analyzers/blob/8236e8bdf092bd9ae21cf42d12b8c480459b5e36/src/Utilities/Compiler/Extensions/ITypeSymbolExtensions.cs#L15-L21
+        // => fromSymbol != null && toSymbol != null && compilation.ClassifyCommonConversion(fromSymbol, toSymbol).IsImplicit;
+        public static bool IsAssignableTo(
+            this ITypeSymbol fromSymbol,
+            ITypeSymbol toSymbol)
+            => fromSymbol != null && toSymbol != null && fromSymbol.AllInterfaces.Concat(fromSymbol.BaseTypesAndSelf()).Any(candidate => candidate.Equals(toSymbol, SymbolEqualityComparer.IncludeNullability));
+
+        public static bool TypeCanAcceptWithNullability(this ITypeSymbol type, ITypeSymbol possiblyNullableTypeToAcceptValueFrom)
+        {
+            if (!type.Equals(possiblyNullableTypeToAcceptValueFrom, SymbolEqualityComparer.Default))
+            {
+                return false;
+            }
+
+            switch (type.NullableAnnotation)
+            {
+                // Type is a non-nullable and can therefore not accept a nullable
+                case NullableAnnotation.NotAnnotated:
+                    return possiblyNullableTypeToAcceptValueFrom.NullableAnnotation == NullableAnnotation.NotAnnotated;
+
+                // Target is a nullable (or not from code that knows about nullable types, so it's nullable) it can accept either nullable or non-nullable
+                case NullableAnnotation.Annotated:
+                case NullableAnnotation.None:
+                    return true;
+
+                default:
+                    throw new ArgumentException("Not expecting a non-annotated type expression.");
+            }
+        }
+    }
+}
