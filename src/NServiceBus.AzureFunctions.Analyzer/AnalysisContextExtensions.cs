1613612ce7e95dafcef88c0b92d6f4e80eb68bad
Tomek Masternak <tomasz.masternak@particular.net>
2023-03-16 18:36:33 +0100

    analyzer scafolding

---
+++ src/NServiceBus.AzureFunctions.Analyzer/AnalysisContextExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace NServiceBus.Core.Analyzer
+{
+    using System.Linq;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    static class AnalysisContextExtensions
+    {
+        public static bool ContainsSyntax(this SyntaxNodeAnalysisContext context, SyntaxNode node)
+        {
+            return node.AncestorsAndSelf().Any(ancestor => ancestor == context.Node);
+        }
+    }
+}
