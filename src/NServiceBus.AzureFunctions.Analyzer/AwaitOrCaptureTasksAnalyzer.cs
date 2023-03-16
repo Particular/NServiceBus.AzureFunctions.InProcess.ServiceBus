d9c74a13e732ad5aa27780e6c372ad348eba6c3d
Tomek Masternak <tomasz.masternak@particular.net>
2023-03-16 18:54:44 +0100

    first tries

--- src/NServiceBus.AzureFunctions.Analyzer/AwaitOrCaptureTasksAnalyzer.cs
+++ src/NServiceBus.AzureFunctions.Analyzer/AwaitOrCaptureTasksAnalyzer.cs
@@ -1,11 +1,8 @@
 namespace NServiceBus.Core.Analyzer
 {
-    using System;
     using System.Collections.Immutable;
-    using System.Linq;
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.CSharp;
-    using Microsoft.CodeAnalysis.CSharp.Syntax;
     using Microsoft.CodeAnalysis.Diagnostics;
 
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
