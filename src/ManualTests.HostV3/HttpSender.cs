196c68959aa6ed47d1c190632b614e24b9474d84
David Boike <david.boike@gmail.com>
2024-03-12 15:24:46 -0500

    And the *other* unused parameter

--- src/ManualTests.HostV3/HttpSender.cs
+++ src/ManualTests.HostV3/HttpSender.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -19,7 +20,7 @@ class HttpSender
     public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest request, ExecutionContext executionContext, ILogger logger)
     {
-        logger.LogInformation("C# HTTP trigger function received a request.");
+        logger.LogInformation("C# HTTP trigger function received a request at {0}", request.GetEncodedPathAndQuery());
 
         var sendOptions = new SendOptions();
         sendOptions.RouteToThisEndpoint();
