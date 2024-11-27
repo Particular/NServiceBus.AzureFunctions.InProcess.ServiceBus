d44ec3d216452c87cfe96971276b613edc587c1f
Brandon Ording <bording@gmail.com>
2024-11-27 16:56:55 -0500

    Clean up trailing whitespace

--- src/NServiceBus.AzureFunctions.InProcess.Analyzer.Tests/OptionsAnalyzerTests.cs
+++ src/NServiceBus.AzureFunctions.InProcess.Analyzer.Tests/OptionsAnalyzerTests.cs
@@ -13,7 +13,7 @@ namespace NServiceBus.AzureFunctions.InProcess.Analyzer.Tests
         public Task DiagnosticIsReportedForOptions(string optionsType, string method, string diagnosticId)
         {
             var source =
-                $@"using NServiceBus; 
+                $@"using NServiceBus;
 class Foo
 {{
     void Bar({optionsType} options)
