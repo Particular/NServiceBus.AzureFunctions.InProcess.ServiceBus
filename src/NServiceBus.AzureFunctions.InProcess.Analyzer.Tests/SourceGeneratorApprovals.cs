c1f920d35eece1da31a938631c89d924fdce1e1b
Brandon Ording <bording@gmail.com>
2024-11-27 17:25:09 -0500

    Move source generator diagnostic descriptors

--- src/NServiceBus.AzureFunctions.InProcess.Analyzer.Tests/SourceGeneratorApprovals.cs
+++ src/NServiceBus.AzureFunctions.InProcess.Analyzer.Tests/SourceGeneratorApprovals.cs
@@ -108,7 +108,7 @@ using NServiceBus;
 ";
             var (_, diagnostics) = GetGeneratedOutput(source, suppressGeneratedDiagnosticsErrors: true);
 
-            Assert.That(diagnostics.Any(d => d.Severity == DiagnosticSeverity.Error && d.Id == TriggerFunctionGenerator.InvalidEndpointNameError.Id), Is.True);
+            Assert.That(diagnostics.Any(d => d.Severity == DiagnosticSeverity.Error && d.Id == AzureFunctionsDiagnostics.InvalidEndpointNameErrorId), Is.True);
         }
 
         [TestCase(null)]
@@ -123,7 +123,7 @@ using NServiceBus;
 ";
             var (_, diagnostics) = GetGeneratedOutput(source, suppressGeneratedDiagnosticsErrors: true);
 
-            Assert.That(diagnostics.Any(d => d.Severity == DiagnosticSeverity.Error && d.Id == TriggerFunctionGenerator.InvalidTriggerFunctionNameError.Id), Is.True);
+            Assert.That(diagnostics.Any(d => d.Severity == DiagnosticSeverity.Error && d.Id == AzureFunctionsDiagnostics.InvalidTriggerFunctionNameErrorId), Is.True);
         }
 
         [Test]
